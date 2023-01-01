f9852d313dcfae53fdb49ba852b6e11e49c1d4da
bracksnow <ktom97@gmail.com>
2023-01-01 20:37:26 +0900

    수정 및 삭제 웹페이지 연결

--- src/main/java/com/bracksnow/practice/springboot/service/PostsService.java
+++ src/main/java/com/bracksnow/practice/springboot/service/PostsService.java
@@ -3,12 +3,17 @@ package com.bracksnow.practice.springboot.service;
 
 import com.bracksnow.practice.springboot.domain.Posts.Posts;
 import com.bracksnow.practice.springboot.domain.Posts.PostsRepository;
+import com.bracksnow.practice.springboot.web.dto.PostsListResponseDto;
 import com.bracksnow.practice.springboot.web.dto.PostsResponseDto;
 import com.bracksnow.practice.springboot.web.dto.PostsSaveRequestDto;
 import com.bracksnow.practice.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
+import org.hibernate.mapping.Collection;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -37,7 +42,12 @@ public class PostsService {
     public void delete (Long id){
         Posts posts = postsRepository.findById(id).orElseThrow(()-> new IllegalArgumentException("해당 게시글이 없습니다. id = "+ id));
         postsRepository.delete(posts);
+    }
 
-
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc(){
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
